2461d84903394353e9b80ca0132ebacc25482fa2
Frank Mitchell <48015596+fmitchell259@users.noreply.github.com>
2019-06-19 20:53:32 +0100

    Update mac_change.py
    Update: Allow the user to input their interface name before changing the MAC address to a random number.

--- mac_changer.py
+++ mac_changer.py
@@ -7,31 +7,31 @@ import random
 def create_mac():
 
     new_mac = " "
-    for x in range(6):
+    for x in range(5):
         rand_int = random.randint(10, 99)
         int_string = str(rand_int)
-        if x == 5:
+        if x == 4:
             new_mac += int_string
         else:
             new_mac += int_string + ":"
-
+    print(new_mac)
     return new_mac
 
 
-def change_mac():
+def change_mac(interface):
 
     n_mac = create_mac()
 
     print("New Mac Address randomly created is: " + str(n_mac))
 
-    subprocess.call(["sudo", "ifconfig", "en0", "down"])
-    subprocess.call(["sudo", "ifconfig", "en0", "ether", n_mac])
-    subprocess.call(["sudo", "ifconfig", "en0", "up"])
+    subprocess.call(["sudo", "ifconfig", interface, "ether", n_mac])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
     print("[+] MAC address changed.")
-    subprocess.call(["sudo", "ifconfig", "en0"])
+    subprocess.call(["sudo", "ifconfig", interface])
 
 
 if __name__ == '__main__':
-    change_mac()
-
+    i_face = input("Please enter your interface name.")
+    change_mac(i_face)
+    
 
